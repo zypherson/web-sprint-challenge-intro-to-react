fb880bbe2f0503da3b64cb2665543075e9ea6986
zypherson <taylorzypherson@gmail.com>
2022-03-25 11:52:58 -0400

    Completed sprint

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
+ 
 const App = () => {
+
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people/")
+    .then(res => {
+      setCharacters(res.data)
+    }).catch(err => console.error(err))
+  } , [])
+ 
+ 
+ 
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -10,9 +24,21 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
+     <h1 className="Header">Characters from Star Wars</h1>
+     {characters.map(character => {
+       return (
+         <div key ={character.name} className='user-wrapper'>
+           <h2>Name: {character.name}</h2>
+           <p>Birthyear: {character.birth_year}</p>
+           <p>Skin Color: {character.skin_color}</p>
+ 
+ 
+         </div>
+       )
+     })}
+   </div>
+ );
 }
+
 
 export default App;
